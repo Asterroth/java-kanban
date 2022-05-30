d70d73479126b2bf060995cedbbb8c0b2c638bc2
Asterroth <anton.malukhin@gmail.com>
2022-05-31 01:39:02 +0300

    feat: Classes Task, Subtask, Epic and TaskManager added

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -3,10 +3,6 @@ import java.util.Map;
 public class TaskManager {
     static Map<Integer, Task> taskList;
 
-    //public TaskManager (Map<Integer, Task> taskList) {
-    //    this.taskList = taskList;
-    //}
-
     public static void setTaskList(Task task) {
         Integer taskID = -1;
         if (taskList != null) {
