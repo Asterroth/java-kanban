46d2490b53cdcc5a2e16f1431e0a0b1a34522848
Asterroth <anton.malukhin@gmail.com>
2022-05-30 21:40:34 +0300

    feat: Classes Task, Subtask, Epic and TaskManager added

---
+++ src/Epic.java
@@ -0,0 +1,6 @@
+public class Epic extends Task {
+
+    public Epic(int taskID, String title, String description, String status) {
+        super(taskID, title, description, status);
+    }
+}
---
+++ src/Subtask.java
@@ -0,0 +1,6 @@
+public class Subtask extends Task {
+
+    public Subtask(int taskID, String title, String description, String status) {
+        super(taskID, title, description, status);
+    }
+}
