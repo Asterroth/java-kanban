32e6434b6881593f182cbfcb2d096b5684988c2e
Asterroth <anton.malukhin@gmail.com>
2022-07-11 20:17:05 +0300

    feat: new version - спринт 5, вер. 1.

--- src/yandex/practicum/kanban/managers/InMemoryHistoryManager.java
+++ src/yandex/practicum/kanban/managers/InMemoryHistoryManager.java
@@ -8,13 +8,12 @@ import java.util.HashMap;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private List<Task> history = new ArrayList<>();
-    private HashMap<Integer, Node> recordedTasks = new HashMap<>();
+    final HashMap<Integer, Node> recordedTasks = new HashMap<>();
 
     public Node head;
     public Node tail;
 
-    public void linkLast(Task element) {  //5/ реализация двусвязного списка задач с методом linkLast
+    public void linkLast(Task element) {
         final Node oldTail = tail;
         final Node newNode = new Node(oldTail, element, null);
         tail = newNode;
@@ -26,19 +25,18 @@ public class InMemoryHistoryManager implements HistoryManager {
         }
     }
 
-    public List<Task> getTasks() { //5/ реализация двусвязного списка задач с методом getTasks
+    public List<Task> getTasks() {
         List<Task> tasks = new ArrayList<>();
         Node currentNode = head;
-        while (!(currentNode == null)) {
+        while (currentNode != null) {
             tasks.add(currentNode.data);
             currentNode = currentNode.next;
         }
         return tasks;
     }
 
-    public void removeNode(Node node) {  //5/ метод void remove(int id) для удаления задачи из просмотра (принимает объект Node — узел связного списка и вырезает его)
-        if (!(node == null)) {
-            final Task data = node.data;
+    public void removeNode(Node node) {
+        if (node != null) {
             final Node next = node.next;
             final Node prev = node.prev;
             node.data = null;
@@ -46,32 +44,28 @@ public class InMemoryHistoryManager implements HistoryManager {
             if (head == node && tail == node) {
                 head = null;
                 tail = null;
-            } else if (head == node && !(tail == node)) {
+            } else if (head == node) {
                 head = next;
                 head.prev = null;
-            } else if (!(head == node) && tail == node) {
+            } else if (tail == node) {
                 tail = prev;
                 tail.next = null;
             } else {
                 prev.next = next;
                 next.prev = prev;
             }
-
-            node = null;
-
         }
-
     }
 
     @Override //5/
     public void addRecord(Task task) {
-        if (!(task == null)) {
+        if (task != null) {
             removeRecord(task.getId());
             linkLast(task);
         }
     }
 
-    @Override  //5/
+    @Override
     public void removeRecord(int id) {
         removeNode(recordedTasks.get(id));
     }
@@ -79,7 +73,7 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public List<Task> getHistory() {
-        return getTasks();  //5/
+        return getTasks();
     }
 }
 
